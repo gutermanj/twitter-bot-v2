10c8c998964348f7cdabcdcbe06f2b7d7d7f669d
Julian Guterman <gutermanj@gmail.com>
2016-03-26 16:33:17 -0400

    Testing

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ var flash = require('connect-flash');
 // Database configuration
 var pg = require('pg');
 
-// var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/twitterbot';
+var connectionString = process.env.DATABASE_URL;
+
+ // || 'postgres://localhost:5432/twitterbot'
 
 // var connectionString = process.env.DATABASE_URL || 'postgres://127.0.0.1:5432/twitterbot';
 

92ba1c9823ec5f57875a89b68234b4df2b0e6081
Julian Guterman <gutermanj@gmail.com>
2016-05-01 17:36:04 -0400

    message count to 20 instead of 5

--- config/messages.js
+++ config/messages.js
@@ -66,7 +66,7 @@ module.exports = {
 	    			timeout_ms: 60 * 1000
 	    		});
 	    		if (account.last_message === null) {
-	    			client.get('direct_messages', { count: 5 }, function(err, messages, response) {
+	    			client.get('direct_messages', { count: 20 }, function(err, messages, response) {
 		    			if (err) {
 		    				console.log("direct_messages", err);
 		    			} else {

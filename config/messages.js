0790b8afb4b24f4f5dfb40bcd3b3051eec5ae7f6
Julian Guterman <gutermanj@gmail.com>
2016-06-21 18:50:12 -0400

    turning back on scheduling

--- config/messages.js
+++ config/messages.js
@@ -391,21 +391,21 @@ module.exports = {
 					} // pushSender
 				}, 1000 * 65 * 1); // Message Pull set Interval
 				console.log("currentQue Started!");
-				// schedule.scheduleJob({
-				// 	hour: 5,
-				// 	minute: 0
-				// }, function() {
-				// 	console.log("Sending Out Morning Rts!");
-				// 	morningMessage();
-				// });
+				schedule.scheduleJob({
+					hour: 5,
+					minute: 0
+				}, function() {
+					console.log("Sending Out Morning Rts!");
+					morningMessage();
+				});
 
-				// schedule.scheduleJob({
-				// 	hour: 7,
-				// 	minute: 0
-				// }, function() {
-				// 	console.log("Sending Out Morning Lmkwd!");
-				// 	morningMessageLmkwd();
-				// });
+				schedule.scheduleJob({
+					hour: 7,
+					minute: 0
+				}, function() {
+					console.log("Sending Out Morning Lmkwd!");
+					morningMessageLmkwd();
+				});
 
 				// schedule.scheduleJob({
 				// 	hour: 1,

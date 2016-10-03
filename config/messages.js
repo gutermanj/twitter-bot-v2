021feceebb0c3634a539631f2cc9305c087ec923
Julian Guterman <gutermanj@gmail.com>
2016-10-03 18:18:01 -0400

    Handle expired tokens

--- config/messages.js
+++ config/messages.js
@@ -149,6 +149,8 @@ module.exports = {
 
 											}
 
+											console.log(err.statusCode);
+
 										} else {
 
 											var query = client.query('UPDATE manualaccounts SET status = $1 WHERE username = $2', [true, account.username]);
@@ -223,11 +225,19 @@ module.exports = {
 									function(err, messages, response) {
 										if (err) {
 											console.log("direct_messages error: " + account.username + ": ", err);
+
 											if (err.statusCode === 403) {
 
 												var query = client.query('UPDATE manualaccounts SET status = $1 WHERE username = $2', [false, account.username]);
 
+											} else if (err.statusCode === 401) {
+
+												var query = client.query('UPDATE manualaccounts SET status = $1 WHERE username = $2', [false, account.username]);
+
 											}
+
+											console.log(err.statusCode);
+											
 
 										} else {
 											if (messages.length < 1) {

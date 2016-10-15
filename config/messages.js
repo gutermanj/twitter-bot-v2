eb6f2f7f4dd9063f6a5c21a9b50b03af191e4bce
Julian Guterman <gutermanj@gmail.com>
2016-10-14 21:03:50 -0400

    found account bug

--- config/messages.js
+++ config/messages.js
@@ -98,11 +98,7 @@ module.exports = {
 
 						// wordfilter.clearList();
 
-						for (i = 0; i < filters.length; i++) {
-							if (splitMessage.indexOf(filters[i]) > -1) {
-								return true;
-							}
-						}
+						
 					}
 
 					function spacedFilter(uppcasedMessage) {
@@ -1095,17 +1091,28 @@ module.exports = {
 
 																console.log(foundAccount);
 
-																if (foundAccount[0].outbound === false) {
-														
-																	addToLmkwdList(currentTrader, account);
+																if (foundAccount.length > 0) {
+
+
+																	if (foundAccount[0].outbound === false) {
+															
+																		addToLmkwdList(currentTrader, account);
+
+																	} else {
+
+																		console.log(currentTrader.sender + " on outbound list for " + account.username);
+
+																		var changeStatus = client.query('UPDATE list SET outbound = $1, history = $2, qued = $5 WHERE sender = $3 AND account_id = $4', [false, true, currentTrader.sender, currentTrader.account_id, false], function(err) {
+																			if (err) return console.log(err);
+																		});
+
+																		var removeFromQue = client.query('DELETE FROM que WHERE sender = $1 AND account_id = $2', [currentTrader.sender, currentTrader.account_id], function(err) {
+																			if (err) return console.log(err);
+																		});
+
+																	}
 
 																} else {
-
-																	console.log(currentTrader.sender + " on outbound list for " + account.username);
-
-																	var changeStatus = client.query('UPDATE list SET outbound = $1, history = $2, qued = $5 WHERE sender = $3 AND account_id = $4', [false, true, currentTrader.sender, currentTrader.account_id, false], function(err) {
-																		if (err) return console.log(err);
-																	});
 
 																	var removeFromQue = client.query('DELETE FROM que WHERE sender = $1 AND account_id = $2', [currentTrader.sender, currentTrader.account_id], function(err) {
 																		if (err) return console.log(err);
